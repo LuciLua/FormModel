9717dc11ea332c07028507a82fefe6a166d8f0ab
LuciLua <luci.lua81@gmail.com>
2021-07-21 21:15:55 -0300

    :pushpin: thanks only after send form

--- js/app.js
+++ js/app.js
@@ -9,13 +9,27 @@ function toggle(){
 function send(){
     var form = document.getElementById('form')
     var toggleBtn = document.getElementById('toggle-btn')
-    form.classList.toggle('form-hidden')  
-    toggleBtn.classList.toggle('toggle-btn-b')
+    var loading = document.getElementById('loading')
 
+    loading.style.display='flex'
+
+    setInterval(function(){
+        loading.style.display='none'
+    }, 2000)
+
+    setInterval(function(){
+        document.getElementById('thanks').style.display='flex'
+    }, 900)
+    
+    
+    setTimeout(function(){
+        form.classList.toggle('form-hidden')  
+        toggleBtn.classList.toggle('toggle-btn-b')
+    },100)
 
     setTimeout(function(){
         window.location.replace(window.location.pathname + '#thanks')
-    }, 100)
+    }, 1000)
 
     
 }
