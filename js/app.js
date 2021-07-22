445acdc8103f9d9360562a56c405ee443eb0d505
LuciLua <luci.lua81@gmail.com>
2021-07-21 22:41:18 -0300

    :pushpin: validation v1

--- js/app.js
+++ js/app.js
@@ -6,30 +6,64 @@ function toggle(){
     toggleBtn.classList.toggle('toggle-btn-b')
 }
 
+const req1 = document.querySelectorAll('.req1')
+const req2 = document.querySelectorAll('.req2')
+var req3 = document.querySelector('.req3')
+
+
+
+
+
 function send(){
     var form = document.getElementById('form')
     var toggleBtn = document.getElementById('toggle-btn')
     var loading = document.getElementById('loading')
 
-    loading.style.display='flex'
+    var totalReq = 0
 
-    setInterval(function(){
-        loading.style.display='none'
-    }, 2000)
 
-    setInterval(function(){
-        document.getElementById('thanks').style.display='flex'
-    }, 900)
+    for(let i = 0; i < req1.length; i++){
+        if( req1[i].checked == true){
+            console.log(req1[i].value,'esta checado')
+            totalReq++
+        }
+    }
+    for(let i = 0; i < req2.length; i++){
+        if( req2[i].checked == true){
+            console.log(req2[i].value,'esta checado')
+            totalReq++
+        }
+    }
+
+        if( req3.value.length != 0){
+            console.log(req3.value,'esta checado')
+            totalReq++
+        }
+
+        console.log(totalReq)
+
+    if (totalReq == 3){ // se todos req forem validos
+        loading.style.display='flex'
+
+        setInterval(function(){
+            loading.style.display='none'
+        }, 2000)
     
+        setInterval(function(){
+            document.getElementById('thanks').style.display='flex'
+        }, 900)
+        
+        
+        setTimeout(function(){
+            form.classList.toggle('form-hidden')  
+            toggleBtn.classList.toggle('toggle-btn-b')
+        },100)
     
-    setTimeout(function(){
-        form.classList.toggle('form-hidden')  
-        toggleBtn.classList.toggle('toggle-btn-b')
-    },100)
+        setTimeout(function(){
+            window.location.replace(window.location.pathname + '#thanks')
+        }, 1000)
+    }
 
-    setTimeout(function(){
-        window.location.replace(window.location.pathname + '#thanks')
-    }, 1000)
 
-    
+
 }
